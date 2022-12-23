b1780494ea1edf5e3fd2552a4ac0ce19ef5e6a55
Elysium Santata <camaleplata@outlook.com>
2022-12-23 09:51:37 -0200

    photo delete

---
+++ src/Componentes/Photo/PhotoDelete.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { PHOTO_DELETE } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import styles from './PhotoDelete.module.css';
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm('Tem certeza que deseja deletar a foto?');
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  }
+  return (
+    <>
+      {loading && loading ? (
+        <button className={styles.delete} disabled>
+          Loading...
+        </button>
+      ) : (
+        <button className={styles.delete} onClick={handleClick}>
+          Deletar
+        </button>
+      )}
+    </>
+  );
+};
+
+export default PhotoDelete;
